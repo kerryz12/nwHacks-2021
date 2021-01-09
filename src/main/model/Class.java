5a124faa6ae53a402a04ea5a6a0c38180070cd95
ker1200 <60584013+ker1200@users.noreply.github.com>
2021-01-09 15:06:59 -0800

    add methods

--- src/main/model/Class.java
+++ src/main/model/Class.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Class {
     private String classCode;
     private ArrayList<Assignment> assignments;
-    private ArrayList<model.Exam> exams;
+    private ArrayList<Exam> exams;
 
     public void addAssignment (String name, int date, int weight) {
         assignments.add(new Assignment(name, date, weight));
