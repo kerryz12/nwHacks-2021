18b956d1740d64e9c94348e5b1cbd8986038b347
ker1200 <60584013+ker1200@users.noreply.github.com>
2021-01-09 23:39:42 -0800

    fixing git issues

--- src/main/ui/UI.java
+++ src/main/ui/UI.java
@@ -3,30 +3,41 @@ package main.ui;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
 import main.model.Class;
 import main.model.ClassList;
 import main.model.GradedItem;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Random;
 
 public class UI extends Application {
     private ClassList classlist;
-    private ArrayList<GradedItem> todolist;
-    private Scanner input;
-    private String user;
+    private ListView<String> list;
+    private ObservableList<String> items;
+
+    private void setup() {
+        classlist = new ClassList();
+        list = new ListView<String>();
+        items = FXCollections.observableArrayList();
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -34,43 +45,108 @@ public class UI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        classlist = new ClassList();
-        Class sampleClass = new Class("MEME 999", 5);
-        classlist.addClass(sampleClass);
-
         primaryStage.setTitle("The Scheduler");
-        Button btn = new Button();
-        btn.setText("Say 'Check Schedule'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Finish Your Assignments!");
+        Group todoGroup = new Group();
+        Group menuGroup = new Group();
+
+        final Random rng = new Random();
+        VBox taskBox = new VBox(5);
+        ScrollPane taskList = new ScrollPane(taskBox);
+        taskList.setFitToWidth(true);
+
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> {
+            AnchorPane anchorPane = new AnchorPane();
+            String style = String.format("-fx-background: rgb(%d, %d, %d);" + "-fx-background-color: -fx-background;",
+                    rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
+            anchorPane.setStyle(style);
+            Label label = new Label("Pane "+(taskBox.getChildren().size()+1));
+            AnchorPane.setLeftAnchor(label, 5.0);
+            AnchorPane.setTopAnchor(label, 5.0);
+            Button button = new Button("Remove");
+            button.setOnAction(evt -> taskBox.getChildren().remove(anchorPane));
+            AnchorPane.setRightAnchor(button, 5.0);
+            AnchorPane.setTopAnchor(button, 5.0);
+            AnchorPane.setBottomAnchor(button, 5.0);
+            anchorPane.getChildren().addAll(label, button);
+            taskBox.getChildren().add(anchorPane);
+        });
+
+        Button refreshButton = new Button("Refresh");
+        refreshButton.setOnAction(e -> {
+            for (Class c : classlist.getClasslist()) {
+                List<GradedItem> tasks = c.getTasks();
             }
         });
 
-        Rectangle rect = new Rectangle(200, 1900, Color.RED);
-        ScrollPane s1 = new ScrollPane();
-        s1.setPrefSize(120, 120);
-        s1.setContent(rect);
+        BorderPane rootPane = new BorderPane(taskList, null, null, null, addButton);
+        Button addClass = new Button();
+        addClass.setText("Add Class");
+        addClass.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Label secondLabel = new Label("I'm a Label on new Window");
+                GridPane grid = new GridPane();
+                grid.setAlignment(Pos.TOP_LEFT);
+                grid.setHgap(10);
+                grid.setVgap(10);
+                grid.setPadding(new Insets(25, 25, 25, 25));
 
-        ClassList sampleList = new ClassList();
-        sampleList.addClass(sampleClass);
-        ObservableList<String> classes = FXCollections.observableArrayList(sampleClass.getClassCode());
-        ListView<String> classList = new ListView<String>(classes);
+                Label codePrompt = new Label("Class Code:");
+                grid.add(codePrompt, 0, 0, 20, 20);
 
-        // gridpane!
-        GridPane gridpane = new GridPane();
+                TextField codeIn = new TextField();
+                grid.add(codeIn, 15, 0,20,20);
 
-        GridPane.setConstraints(s1, 0, 0); // column=2 row=0
-        GridPane.setConstraints(btn, 2, 0); // column=2 row=0
-        GridPane.setConstraints(classList, 5, 0);
+                Label importancePrompt = new Label("Importance:");
+                grid.add(importancePrompt, 0, 5, 20, 20);
 
-        // don't forget to add children to gridpane
-        gridpane.getChildren().addAll(s1, btn, classList);
-        // gridpane!
+                TextField importanceIn = new TextField();
+                grid.add(importanceIn, 15, 5,20,20);
 
-        primaryStage.setScene(new Scene(gridpane, 1600, 900));
+                Button submit = new Button();
+                submit.setText(" Submit ");
+                grid.add(submit, 28,10,20,20);
+                submit.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        if (!classlist.contains(codeIn.getCharacters().toString())) {
+                            classlist.addClass(new Class(codeIn.getCharacters().toString(), Integer.parseInt(importanceIn.getCharacters().toString())));
+                        }
+                        else {
+                            //class already added. do something.
+                        }
+                    }
+                });
+
+                Scene secondScene = new Scene(grid, 400, 300);
+
+                // New window (Stage)
+                Stage newWindow = new Stage();
+                newWindow.setTitle("Add Class");
+                newWindow.setScene(secondScene);
+
+                // Specifies the modality for new window.
+                newWindow.initModality(Modality.WINDOW_MODAL);
+
+                // Specifies the owner Window (parent) for new window
+                newWindow.initOwner(primaryStage);
+
+                // Set position of second window, related to primary window.
+                newWindow.setX(primaryStage.getX() + 200);
+                newWindow.setY(primaryStage.getY() + 100);
+
+                newWindow.show();
+            }
+        });
+
+        rootPane.getChildren().addAll(taskBox);
+
+        //when the scene is created, it should just render all the groups
+
+        Scene scene = new Scene(rootPane, 1600, 900);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
