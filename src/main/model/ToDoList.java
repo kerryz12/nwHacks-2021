e2d19834ef5f0966f1368f7ad5c078e4af849f18
Henrique Saito <hsaito2002@gmail.com>
2021-01-09 17:10:28 -0800

    iteration 1 of ToDoList

--- src/main/model/ToDoList.java
+++ src/main/model/ToDoList.java
@@ -1,5 +1,6 @@
 package main.model;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ToDoList {
@@ -15,8 +16,8 @@ public class ToDoList {
 
     private void sortList(){
         currTime = System.currentTimeMillis();
-        List<Class> classlist = classes.getList();
-        weightedTasks = new HashMap<>();
+        List<Class> classlist = classes.getClasslist();
+        weightedTasks = new LinkedHashMap<>();
         for (Class c : classlist){
             int curr = c.getImp();
             List<GradedItem> tasks = c.getTasks();
@@ -27,24 +28,39 @@ public class ToDoList {
             }
         }
         weightedTasks = sortByValue(weightedTasks);
-
+        sortedList.addAll(weightedTasks.keySet());
     }
 
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-        list.sort(new Comparator<Map.Entry<K, V>>() {
+    public static <GradedItem, Long> Map<GradedItem, Long> sortByValue(Map<GradedItem, Long> map) {
+        List<Map.Entry<GradedItem, Long>> list = new ArrayList<>(map.entrySet());
+        list.sort(new Comparator<Map.Entry<GradedItem, Long>>() {
             @Override
-            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-                return 0;
+            //if o1 is greater, return 1, if o2 is greater, return -1.
+            // greater in this case, is defined as having a lesser value.
+            public int compare(Map.Entry<GradedItem, Long> o1, Map.Entry<GradedItem, Long> o2) {
+                if (((Number)o1.getValue()).longValue() > ((Number)o2.getValue()).longValue()){
+                    return -1;
+                }
+                else if (((Number)o1.getValue()).longValue() < ((Number)o2.getValue()).longValue()){
+                    return 1;
+                }
+                else {
+                    return 0;
+                }
             }
         });
-        Map<K, V> result = new LinkedHashMap<>();
-        for (Map.Entry<K, V> entry : list) {
+        Map<GradedItem, Long> result = new LinkedHashMap<>();
+        for (Map.Entry<GradedItem, Long> entry : list) {
             result.put(entry.getKey(), entry.getValue());
         }
         return result;
     }
 
+    public ArrayList<GradedItem> getSortedList() { return sortedList; }
 
-
+    public static ArrayList<GradedItem> getToDoList(ClassList cl){
+        ToDoList t = new ToDoList(cl);
+        t.sortList();
+        return t.getSortedList();
+    }
 }
